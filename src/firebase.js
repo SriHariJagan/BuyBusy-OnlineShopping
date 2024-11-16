8ab411d139818b1e691d62dba85af2e4b1002cad
Sri Hari Jagan <100404902+SriHariJagan@users.noreply.github.com>
2024-11-16 12:16:59 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBXVjuJgCu9XAwp_ZZQyiHpOvBg6M7_iZM",
-  authDomain: "buybusy-4dc32.firebaseapp.com",
-  projectId: "buybusy-4dc32",
-  storageBucket: "buybusy-4dc32.firebasestorage.app",
-  messagingSenderId: "775577330296",
-  appId: "1:775577330296:web:ff44b9eba7eaf4a5cd838d"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 const auth = getAuth(app);
 
 export { db, auth };
